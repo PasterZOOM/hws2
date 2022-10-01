0c8c9ca99b684f55d95914b5231015dddcf0318b
Ilya Pasternak <PasterZOOM@gmail.com>
2022-10-01 15:40:52 +0300

    hw10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,11 +1,16 @@
+type LoadingInitialStateType = {
+    isLoading: boolean
+}
+
 const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state: LoadingInitialStateType = initState, action: LoadingActionType): LoadingInitialStateType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING':
+            return {...state, isLoading: action.isLoading}
         default:
             return state
     }
